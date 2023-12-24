78d8eb52b83fbd273e9ed348aa8fe6d3fe2ce6d6
Flamewiing <niklas-hohn@t-online.de>
2023-12-24 13:20:06 +0100

    edit vim settings and reformat sorting file

--- gdi/algorithms/sorting.py
+++ gdi/algorithms/sorting.py
@@ -1,43 +1,44 @@
-from collections import deque
 import pytest
 import random
 
 
 """
 Attribute von Sortieralgoritmen
-
     Rekursiv
     Returned kopie der Liste
     Laufzeitkomplexität
     Speicherkomplexität
-
-
-
 """
 
-def bubble_sort(lst:list): #* O(n**2)
+
+def bubble_sort(lst: list):
     swapped = True
-    while swapped: 
+    while swapped:
         swapped = False
         for j in range(len(lst)-1):
             if lst[j] > lst[j+1]:
-                lst[j], lst[j+1] = lst[j+1], lst[j] 
-                
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+                swapped = True
+    return lst
 
-def selection_sort(lst:list): #* O(n**2)
+
+def selection_sort(lst: list):  # O(n**2)
     min_number = -float("inf")
 
-def insertion_sort(lst:list): #* O(n**2)
+
+def insertion_sort(lst: list):  # O(n**2)
     ...
 
-def quick_sort(lst:list): #* O(n * log(n))
+
+def quick_sort(lst: list):  # O(n * log(n))
     ...
 
-def merge_sort(lst:list): #* O(n * log(n))
+
+def merge_sort(lst: list):  # O(n * log(n))
     left = lst[:len(lst)//2]
     if len(left) == 1:
         return left[0]
-    
+
     right = lst[len(lst)//2:]
     if len(right) == 1:
         return right[0]
@@ -45,7 +46,12 @@ def merge_sort(lst:list): #* O(n * log(n))
     return left+right
 
 
-@pytest.mark.parametrize("f",[bubble_sort, selection_sort, insertion_sort, merge_sort, quick_sort])
+@pytest.mark.parametrize("f",
+                         [bubble_sort,
+                          selection_sort,
+                          insertion_sort,
+                          merge_sort,
+                          quick_sort])
 def test_sorts(f):
     lst = [i for i in range(10)]
     random.shuffle(lst)
