7be99082e512bab9e9f035c000e4b0628d89caf0
Flamewiing <niklas-hohn@t-online.de>
2024-01-15 16:11:52 +0100

    DailyCommit 2024/01/15 15:11

---
+++ data1/s-nhohnn_kw43_mindmap_2024-01-12.mm
@@ -0,0 +1,161 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Frick-DataGovernance" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1705047461383">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="0.25">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Messen und Beobachten" POSITION="bottom_or_right" ID="ID_1467368748" CREATED="1705047489586" MODIFIED="1705047747648">
+<node TEXT="Wie?" ID="ID_315917756" CREATED="1705047747652" MODIFIED="1705047754852">
+<node TEXT="Kontinuierlich / Regelmäßig" ID="ID_1271352997" CREATED="1705047872699" MODIFIED="1705047891637"/>
+<node TEXT="Durch Zielsetzung + Aktueller Stand" ID="ID_1566464513" CREATED="1705047892622" MODIFIED="1705047931930"/>
+</node>
+<node TEXT="Wraum?" ID="ID_1297338192" CREATED="1705047775956" MODIFIED="1705047778874">
+<node TEXT="Verbesserung" ID="ID_1911343223" CREATED="1705047936662" MODIFIED="1705048109209"/>
+<node TEXT="Weiterentwicklung" ID="ID_1159133957" CREATED="1705048110173" MODIFIED="1705048114638"/>
+<node TEXT="Abweichungserkennung" ID="ID_199350505" CREATED="1705048114932" MODIFIED="1705048129882"/>
+<node TEXT="Problemerkennung" ID="ID_995009546" CREATED="1705048135046" MODIFIED="1705048143441"/>
+<node TEXT="Strategiereflexion" ID="ID_1797739913" CREATED="1705048144575" MODIFIED="1705048152576"/>
+</node>
+</node>
+<node TEXT="Technologie" POSITION="bottom_or_right" ID="ID_983948209" CREATED="1705047790577" MODIFIED="1705047818369">
+<node TEXT="Was muss betrachtet werden?" ID="ID_763892673" CREATED="1705048157681" MODIFIED="1705048303415">
+<node TEXT="Datenschutz" ID="ID_1232140905" CREATED="1705048304217" MODIFIED="1705048307233"/>
+<node TEXT="Datensicherheit" ID="ID_568007069" CREATED="1705048307463" MODIFIED="1705048311704"/>
+<node TEXT="Datenqualitätsmanagement" ID="ID_908192380" CREATED="1705048311867" MODIFIED="1705048319399"/>
+</node>
+<node TEXT="Wie gelingt die Umsetzung?" ID="ID_927324651" CREATED="1705048323631" MODIFIED="1705048333902">
+<node TEXT="Bereitstellung geeigneter Werkzeuge" ID="ID_813109864" CREATED="1705048333907" MODIFIED="1705048353109"/>
+<node TEXT="Schulungen für Mitarbeiter" ID="ID_1913150364" CREATED="1705048353597" MODIFIED="1705048359029"/>
+<node TEXT="Übersicht der Daten" ID="ID_1288430931" CREATED="1705048359467" MODIFIED="1705048373074"/>
+<node TEXT="Management der Metadaten" ID="ID_558488332" CREATED="1705048373630" MODIFIED="1705048382882">
+<node TEXT="Bedeutung der Informationsobjekte" ID="ID_1822884694" CREATED="1705048392739" MODIFIED="1705048406466"/>
+<node TEXT="Prozessinformationen bzgl. Veränderung, Verknüprung, Zuordnung" ID="ID_1843122656" CREATED="1705048408902" MODIFIED="1705048487175"/>
+<node TEXT="Strukturangaben bzgl. Datentyp, Wertebereich, Qualität" ID="ID_228065700" CREATED="1705048441500" MODIFIED="1705048500773"/>
+<node TEXT="Administrative Informationen über Erstellungszeitpunkt, Zugriffhäufigkeit, Berechtigung" ID="ID_1307807844" CREATED="1705048503359" MODIFIED="1705048622421"/>
+</node>
+</node>
+<node TEXT="Richtiges Data-Management" ID="ID_362096166" CREATED="1705048628292" MODIFIED="1705048638304">
+<node TEXT="Warum?" ID="ID_809583731" CREATED="1705049254690" MODIFIED="1705049260982">
+<node TEXT="Zentrale Datenspeicherung für entwickler" ID="ID_159459966" CREATED="1705049260987" MODIFIED="1705049281193"/>
+<node TEXT="Klassifizierung u. Anreicherung der Daten" ID="ID_1231451797" CREATED="1705049282321" MODIFIED="1705049440852"/>
+<node TEXT="DataLake erstellung durch unstrukturierte Daten" ID="ID_1662796334" CREATED="1705049441957" MODIFIED="1705049462593"/>
+</node>
+<node TEXT="Data Lineage (Herkunft)" ID="ID_446143016" CREATED="1705048638310" MODIFIED="1705048657774">
+<node TEXT="Aus aggregierten Datensätzen die Ursprünglichen Datensätze bestimmen" ID="ID_1653593679" CREATED="1705048666602" MODIFIED="1705049204262"/>
+</node>
+<node TEXT="Data Catalog" ID="ID_1713804425" CREATED="1705049211040" MODIFIED="1705049216281">
+<node TEXT="Beschreibung der gespeicherten Daten" ID="ID_598251496" CREATED="1705049216287" MODIFIED="1705049236164"/>
+</node>
+</node>
+</node>
+<node TEXT="Kommunikation" POSITION="bottom_or_right" ID="ID_321059997" CREATED="1705047819289" MODIFIED="1705047832589">
+<node TEXT="Wie?" ID="ID_1170311052" CREATED="1705049589668" MODIFIED="1705049615099">
+<node TEXT="Strukturierter Informationsaustausch" POSITION="bottom_or_right" ID="ID_1233413880" CREATED="1705049544741" MODIFIED="1705049559209"/>
+<node TEXT="Frühzeitige Informationsweitergabe" POSITION="bottom_or_right" ID="ID_1005191706" CREATED="1705049529260" MODIFIED="1705049544033"/>
+<node TEXT="Zielgruppenorientiert" POSITION="bottom_or_right" ID="ID_474630311" CREATED="1705049491888" MODIFIED="1705049514175"/>
+<node TEXT="Einhaltung von Richtlienien und Regeln" POSITION="bottom_or_right" ID="ID_1293407333" CREATED="1705049515077" MODIFIED="1705049528831"/>
+</node>
+<node TEXT="Umsetzung" ID="ID_402761146" CREATED="1705049619596" MODIFIED="1705049730903">
+<node TEXT="Kommunikationsplan" ID="ID_917164206" CREATED="1705049730905" MODIFIED="1705049739249">
+<node TEXT="Wer benötigt Informationen" ID="ID_642254373" CREATED="1705049739254" MODIFIED="1705049747070"/>
+<node TEXT="Wer ist verantwortlich" ID="ID_1973016478" CREATED="1705049766362" MODIFIED="1705049772303"/>
+</node>
+<node TEXT="Trainingsplan" ID="ID_267497141" CREATED="1705049774060" MODIFIED="1705049779076">
+<node TEXT="Schulungsplan für involvierte Gruppen/Personen" ID="ID_546727795" CREATED="1705049779451" MODIFIED="1705049792757"/>
+</node>
+</node>
+</node>
+<node TEXT="DataQuality Management" POSITION="bottom_or_right" ID="ID_1180327800" CREATED="1705047850931" MODIFIED="1705050009329">
+<font SIZE="16"/>
+<node TEXT="Definition" POSITION="bottom_or_right" ID="ID_1319347510" CREATED="1705049891617" MODIFIED="1705049895169">
+<node TEXT="Themenbereich der sich mit dem Arbeiten, mit qualitativ hochwertigen Daten befasst." POSITION="bottom_or_right" ID="ID_1121350396" CREATED="1705049799440" MODIFIED="1705049883099"/>
+</node>
+<node TEXT="Warum?" POSITION="bottom_or_right" ID="ID_125874134" CREATED="1705049899888" MODIFIED="1705049910628">
+<node TEXT="Daten haben wirtschaftliches Potenzial" ID="ID_1031427454" CREATED="1705049911207" MODIFIED="1705049925706"/>
+<node TEXT="Daten sind oft fehlerhaft, widersprüchlich, unvollständig oder veraltet" ID="ID_1900741763" CREATED="1705049926155" MODIFIED="1705049963502"/>
+</node>
+<node TEXT="Wie?" POSITION="bottom_or_right" ID="ID_828010009" CREATED="1705049968964" MODIFIED="1705049971253">
+<node TEXT="Validierung" ID="ID_853562360" CREATED="1705049971258" MODIFIED="1705049976671"/>
+<node TEXT="Standardisierung" ID="ID_486861061" CREATED="1705049977265" MODIFIED="1705049982470"/>
+<node TEXT="Bereinigung" ID="ID_1754469257" CREATED="1705049983216" MODIFIED="1705049987300"/>
+<node TEXT="Anreicherung" ID="ID_135352926" CREATED="1705049987766" MODIFIED="1705049991674"/>
+</node>
+</node>
+</node>
+</map>
